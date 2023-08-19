39160be2908ee74b086971012c5e54e339b699e6
marce <cmnxx14@gmail.com>
2023-08-19 11:41:24 +0700

    init

---
+++ cookie_test.go
@@ -0,0 +1,16 @@
+package Golang_web
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func SetCookie(writer http.ResponseWriter, request *http.Request) {
+	cookie := new(http.Cookie)
+	cookie.Name = "X-PZN-Name"
+	cookie.Value = request.URL.Query().Get("name")
+	cookie.Path = "/"
+
+	http.SetCookie(writer, cookie)
+	fmt.Fprint(writer, "Success create cookie")
+}
